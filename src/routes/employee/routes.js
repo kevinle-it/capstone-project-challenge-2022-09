956172cf8593ad7e24d50385ac9ad9c55b113ad5
Tri Le <trile95.it@gmail.com>
2022-10-06 14:38:19 -0400

    refactor respond function and response data

--- src/routes/employee/routes.js
+++ src/routes/employee/routes.js
@@ -1,34 +1,17 @@
 import express from 'express';
+import { respond } from '../../helpers';
 import { login, registerEmployee } from './controllers.js';
 
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
   const json = await registerEmployee(req.body);
-
-  if (json.status === 200) {
-    res.status(json.status)
-       .header(json.header)
-       .send(json.data);
-  } else {
-    console.error(json);
-    res.status(json.status)
-       .send(json.data.message);
-  }
+  respond(json, res);
 });
 
 router.post('/login', async(req, res) => {
   const json = await login(req.body);
-
-  if (json.status === 200) {
-    res.status(json.status)
-       .header(json.header)
-       .send(json.data);
-  } else {
-    console.error(json);
-    res.status(json.status)
-       .send(json.data.message);
-  }
+  respond(json, res);
 });
 
 export default router;
